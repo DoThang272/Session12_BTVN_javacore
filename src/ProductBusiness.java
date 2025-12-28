9d906d5e03807c273b0c03a0bd815871035094fe
DoThang <doanhthang16@gmail.com>
2025-12-28 12:23:25 +0700

    update bt1

--- src/ProductBusiness.java
+++ src/ProductBusiness.java
@@ -4,14 +4,15 @@ public class ProductBusiness {
 
     public static Product[] products = new Product[100];
     public static int currentIndex = 0;
+
     public static int getNextId() {
         int maxId = 0;
         for (int i = 0; i < currentIndex; i++) {
-            if(products[i].getProductId() > maxId){
+            if (products[i].getProductId() > maxId) {
                 maxId = products[i].getProductId();
             }
         }
-        return maxId+1;
+        return maxId + 1;
     }
 
     public static void main(String[] args) {
@@ -43,8 +44,10 @@ public class ProductBusiness {
                     deletePById(sc);
                     break;
                 case 5:
+                    searchProductByName(sc);
                     break;
                 case 6:
+                    sortProductESCPrice();
                     break;
                 case 7:
                     break;
@@ -60,7 +63,7 @@ public class ProductBusiness {
     }
 
     public static void displayProduct() {
-        if (currentIndex == 0){
+        if (currentIndex == 0) {
             System.out.println("Hava 0 product, Let add product.");
         }
         for (int i = 0; i < currentIndex; i++) {
@@ -75,33 +78,35 @@ public class ProductBusiness {
         products[currentIndex] = p;
         currentIndex++;
     }
-    public static int findIndexById(int productID){
+
+    public static int findIndexById(int productID) {
         for (int i = 0; i < currentIndex; i++) {
-            if (productID == products[i].getProductId()){
+            if (productID == products[i].getProductId()) {
                 return i;
             }
         }
         return -1;
     }
-    public static void deletePById(Scanner sc){
+
+    public static void deletePById(Scanner sc) {
         System.out.print("Enter the id of product you want to delete: ");
         int idToDelete = Integer.parseInt(sc.nextLine());
         int idxProductDelete = findIndexById(idToDelete);
-        if (idxProductDelete == -1){
+        if (idxProductDelete == -1) {
             System.err.println("Not found!");
             return;
         }
         for (int i = idxProductDelete; i < currentIndex; i++) {
-            products[i] = products[i+1];
+            products[i] = products[i + 1];
         }
         currentIndex--;
     }
 
-    public static void updateProduct(Scanner sc){
+    public static void updateProduct(Scanner sc) {
         System.out.print("Enter the id of product you want to update: ");
         int idToUpdate = Integer.parseInt(sc.nextLine());
         int idxProductUpdate = findIndexById(idToUpdate);
-        if (idxProductUpdate == -1){
+        if (idxProductUpdate == -1) {
             System.err.println("Not found this product.");
             return;
         }
@@ -115,7 +120,7 @@ public class ProductBusiness {
         System.out.print("Your choice: ");
         int choice = Integer.parseInt(sc.nextLine());
 
-        switch (choice){
+        switch (choice) {
             case 1:
                 products[idxProductUpdate].setProductName(updateName(sc));
                 break;
@@ -123,9 +128,12 @@ public class ProductBusiness {
                 products[idxProductUpdate].setPrice(updatePriceOProduct(sc));
                 break;
             case 3:
-
+                products[idxProductUpdate].setCategory(updateCategory(sc));
                 break;
             case 4:
+                products[idxProductUpdate].setQuantity(updateQuantity(sc));
+                break;
+            case 5:
                 break;
             default:
                 System.err.println("You must choose from 1 to 5.");
@@ -134,14 +142,47 @@ public class ProductBusiness {
 
     }
 
-    public static String updateName(Scanner sc){
+    public static String updateName(Scanner sc) {
         System.out.print("Input new name of product: ");
         return sc.nextLine();
     }
-    public static float updatePriceOProduct(Scanner sc){
+
+    public static float updatePriceOProduct(Scanner sc) {
         System.out.print("Input new price of product: ");
         return Integer.parseInt(sc.nextLine());
     }
-//    public static String
 
+    public static String updateCategory(Scanner sc) {
+        System.out.print("Input new category: ");
+        return sc.nextLine();
+    }
+
+    public static int updateQuantity(Scanner sc) {
+        System.out.print("Input new quantity in stock: ");
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    public static void searchProductByName(Scanner sc) {
+        System.out.print("Enter the name product you want to search: ");
+        String nameSearch = sc.nextLine();
+        for (int i = 0; i < currentIndex; i++) {
+            if (products[i].toString().toLowerCase().contains(nameSearch)) {
+                products[i].displayData();
+            }
+        }
+    }
+
+    public static void sortProductESCPrice() {
+        for (int i = 0; i < currentIndex; i++) {
+            float temp;
+            if (products[i].getPrice() > products[i + 1].getPrice()) {
+                temp = products[i].getPrice();
+                products[i].setPrice(products[i + 1].getPrice());
+                products[i + 1].setPrice(temp);
+            }
+        }
+        for (int i = 0; i < currentIndex; i++) {
+            products[i].displayData();
+        }
+    }
 }
